1dfc9a37cfaec77bb5ee8f4e722a13ec6bfafc4f
devolexa <starovatovasasha@gmail.com>
2025-11-29 14:08:19 +0200

    fix 2.0

--- src/main.js
+++ src/main.js
@@ -14,11 +14,9 @@ import "izitoast/dist/css/iziToast.min.css";
 const form = document.querySelector(".form");
 const loadMoreBtn = document.querySelector(".load-more-btn");
 
-
 //* EVENT
 
-
-let page = 0;
+let page = 1;
 let prevSearch = "";
 let totalLoaded = 0;
 
@@ -27,6 +25,7 @@ form.addEventListener("submit", async (e) => {
     hideLoadMoreButton();
 
     const search = e.target.elements['search-text'].value.trim();
+
     if (!search) {
         iziToast.info({
             message: 'Please type something to start searching.',
@@ -34,18 +33,19 @@ form.addEventListener("submit", async (e) => {
         });
         return;
     } 
+
     if (search !== prevSearch) {
-        page = 0;
         prevSearch = search;
         totalLoaded = 0;
         clearGallery()
     }    
-    page += 1;
+
+    page = 1;
+
     showLoader()
 
     try {
         const data = await getImagesByQuery(search, page)
-        hideLoader();
         if (page === 1 && data.hits.length === 0) {
             iziToast.error({
                 message: 'Sorry, there are no images matching your search query. Please try again!',
@@ -65,26 +65,29 @@ form.addEventListener("submit", async (e) => {
             hideLoadMoreButton()
             return;
         }
+
         showLoadMoreButton()
     }
 
     catch(error) {
-        hideLoader();
         iziToast.error({
             title: 'Error',
             message: 'Something went wrong. Please try again.'
         });
-    };
+    }
+
+    finally {
+        hideLoader();
+    }
 })
 
 loadMoreBtn.addEventListener("click", async (e) => {
-    showLoader()
-    hideLoadMoreButton()
     page += 1;
-    
+    hideLoadMoreButton()
+    showLoader()
+
     try {
         const data = await getImagesByQuery(prevSearch, page);    
-        hideLoader()
         createGallery(data.hits)
         const firstCard = document.querySelector('.gallery-item');
         if (firstCard) {
@@ -95,21 +98,24 @@ loadMoreBtn.addEventListener("click", async (e) => {
             });
         }
         totalLoaded += data.hits.length;
-        if (totalLoaded >= data.totalHits) {
+        if (totalLoaded < data.totalHits) {
+            showLoadMoreButton();
+        } else {
             iziToast.info({
                 message: "We're sorry, but you've reached the end of search results.",
                 position: 'topRight'
-            })
-            hideLoadMoreButton()
-            return;
+            });
         }
-        showLoadMoreButton()
     }   
+
     catch (error) {
-        hideLoader();
         iziToast.error({
             title: 'Error',
             message: 'Something went wrong. Please try again.'
         });
     }    
+
+    finally {
+        hideLoader();
+    }
 })
